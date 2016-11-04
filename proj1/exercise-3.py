947064222d136291ff638f49e2a35c0e9fcf6ed1
carboned4 <slf450x@hotmail.com>
2016-11-04 12:57:28 +0000

    nomes dos ficheiros

--- proj1/exercise-3.py
+++ proj1/exercise-3.py
@@ -4,140 +4,158 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.collocations import *
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-from sklearn.metrics import f1_score
-
+from sklearn.feature_extraction.text import CountVectorizer
 import operator
+import re
+from math import log10
 import numpy
 
-    
-def sort(array):
-    less = []
-    equal = []
-    greater = []
 
-    if len(array) > 1:
-        pivot = array[0]
-        for x in array:
-            if x < pivot:
-                less.append(x)
-            if x == pivot:
-                equal.append(x)
-            if x > pivot:
-                greater.append(x)
-        # Don't forget to return something!
-        return sort(less)+equal+sort(greater)  # Just use the + operator to join lists
-    # Note that you want equal ^^^^^ not pivot
-    else:  # You need to hande the part at the end of the recursion - when you only have one element in your array, just return the array.
-        return array
+#general statistics that we'll calculate further down
+numberofdocuments = 0
+totalwordsperdocument = dict()
+totalwordsincorpus = 0
 
+
+#checking if a term (uni/bi/trigram) matches a regex.
+#conversao passo-a-passo da gramatica do enunciado
+#grammar2 = r'(<NN[A-Z]*>)+$'
+#grammar2 = r'(<JJ> <NN[A-Z]*>)+$'
+#grammar2 = r'((<JJ>)* <NN[A-Z]*>)+$'
+#grammar2 = r'((<IN>)?(<JJ>)*<NN[A-Z]*>)+$'
+#grammar2 = r'((<IN>)?(<JJ>)*(<NN[A-Z]*>)+)+$'
+grammar2 = r'(((<JJ>)*(<NN[A-Z]*>)+<IN>)?(<JJ>)*(<NN[A-Z]*>)+)+$'
+regexparser2 = re.compile(grammar2)
+def filterCandidates(candidatesList):
+    newCandidates = []
+    for candidate in candidatesList:
+        stringtocheck = ""
+        if isinstance(candidate, tuple):
+            candidate = " ".join(candidate)
+        candidatetags = nltk.pos_tag(nltk.word_tokenize(candidate))
+        for taggedterm in candidatetags:
+            stringtocheck += "<"+taggedterm[1]+">"
+        parseResult = regexparser2.match(stringtocheck)
+        if parseResult:
+            newCandidates += [candidate]
+    return newCandidates
+
+
+#CUSTOM TOKENIZER
+#this tokenizer will split words, delete stop words and punctuation and
+#transform it into uni/bi/trigrams, which will all be filtered by the regex.
+#some document and general statistics are also calculated here.
+punct = string.punctuation
+punct = punct.translate(None,"'")
+punctexcludeset = set(punct)
+stop = set(stopwords.words('english'))
+docindex = 0
+def test_set(s):
+    return ''.join(ch for ch in s if ch not in punctexcludeset)
+def my_tokenizer(documentasstring):
+    #these variables have to be declared here as global so the vectorizer can
+    #see them from our program (we first declared them earlier)
+    global docindex
+    global totalwordsincorpus
+    global numberofdocuments
+    docsentences = nltk.sent_tokenize(documentasstring)
+    docwords = list()
+    doclength = 0
+    for docsentence in docsentences:
+        sentencenopunct = test_set(docsentence.lower())
+        sentencewords = nltk.word_tokenize(sentencenopunct)
+        doclength += len(sentencewords)
+        for i in range(len(sentencewords)):
+            word = sentencewords[i]
+            if word[0] == "'":
+                word = word[1:]
+            sentencewords[i] = word
+        sentenceclean = [i for i in sentencewords if i not in stop]
+        docwords += sentenceclean
+    totalwordsperdocument[docindex] = doclength
+    totalwordsincorpus += doclength
+    #all the words are split
+    docbigrams = list(nltk.bigrams(docwords))
+    doctrigrams = list(nltk.trigrams(docwords))
+    docterms = docwords + docbigrams + doctrigrams
+    docvalidterms = filterCandidates(docterms)
+    docindex +=1
+    numberofdocuments +=1
+    return docvalidterms
+
+#PROCESSING:
+
+#documents from FAO30 (same as exercise-2)
 
 import os
 path = "fao30/documents/"
 
 all_docs = []
 docindexnames = dict()
-docindex = 0
+docreadindex = 0
 for filename in os.listdir(path):
-    docindexnames[docindex] = filename
+    docindexnames[docreadindex] = filename
     etd = open(path + filename)
     etdread = etd.read()
     etdread = etdread.decode('latin-1')
     etd_words = nltk.word_tokenize(etdread)
     all_docs += [etdread]
-    docindex += 1
+    docreadindex += 1
 
-stop = set(stopwords.words('english'))
 
-vectorizer2 = TfidfVectorizer( use_idf=True, min_df=1,ngram_range=(1,3), stop_words=stop, analyzer='word', token_pattern='{(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}+' )
-docstfidf = vectorizer2.fit_transform(all_docs)
-vecvocab = vectorizer2.vocabulary_
 
-####################
+#uses a vectorizer to calculate term frequency
+countVectorizer = CountVectorizer(tokenizer=my_tokenizer)
+countVectorizer.build_analyzer()
+#docstf = countVectorizer.fit_transform(set(["Alice stopped by the big big station to retrieve the blue poop","Alice stopped by a poop and was angry"]))
+docstf = countVectorizer.fit_transform(all_docs)
+vecvocab = countVectorizer.vocabulary_
 
-path = "fao30/indexers/iic1/"
 
-keysfordoc = dict()
-for filename in os.listdir(path):
-    etd = open(path + filename)
-    etdread = etd.read()
-    etdread = etdread.decode('latin-1')
-    etd_keys = etdread.split("\n",-1)[:-1]
-    filename = filename[:-4] + '.txt'
-    keysfordoc[filename] = etd_keys
+
+#calculates the IDF according to the new formula
+idfDict = dict()
+for termi in range(len(vecvocab)):
+    docswithterm = docstf.getcol(termi).getnnz(0)[0]
+    numerator = numberofdocuments - docswithterm +0.5
+    denominator = docswithterm +0.5
+    idfDict[termi] = log10(numerator/denominator)
+
+
+#calculates the BM25 for a term,document
+k1 = 1.2
+b = 0.75
+def score(documentn, termi):
+    idfpart = idfDict[termi]
+    ftD = docstf[documentn,termi]
+    avgdl = totalwordsincorpus/(0.0+numberofdocuments)
+    #print ftD
+    numeratorpart = ftD * (k1 + 1)
+    denominatorpart = ftD + k1 * (1 - b + b * (totalwordsperdocument[documentn]/avgdl) )
+    scoredt = idfpart * (numeratorpart / denominatorpart)
+    return scoredt
+
+
+#all the scores will be calculated here
+dictscores = dict()
+for doci in range(numberofdocuments):
+    documentscores = dict()
+    for termi in range(len(vecvocab)):
+        documentscores[termi] = score(doci, termi)
+    dictscores[doci] = documentscores
 
 
 doccandidateslist = dict()
-featurenames = list(vectorizer2.get_feature_names())
-for idoc in range(len(docindexnames)):
-    docname = docindexnames[idoc]
-    featurenamescopy = numpy.array(featurenames)
-    tfidfdoccopy = numpy.array(docstfidf.getrow(idoc).toarray()[0])
-    sortedindices = (tfidfdoccopy.argsort()[-5:])[::-1]
+featurenames = list(countVectorizer.get_feature_names())
+featurenamescopy = numpy.array(featurenames)
+for idoc in range(numberofdocuments):
+    bm25doccopy = numpy.array(dictscores[idoc].values())
+    #the keys were inserted by order, so the values were by this order as well
+    sortedindices = (bm25doccopy.argsort()[-5:])[::-1]
     candidatewordsfordoc = list()
     for candidatei in sortedindices:
         candidatewordsfordoc += [featurenamescopy[candidatei]]
-    doccandidateslist[docname] = candidatewordsfordoc
+    doccandidateslist[idoc] = candidatewordsfordoc
     #print docname
     #print candidatewordsfordoc
     
-
-    
-
-measuresdoc = dict()
-for idoc in range(len(docindexnames)):
-    measures= dict()
-    docname = docindexnames[idoc]
-    setrelevant = set(keysfordoc[docname])
-    setanswer = set(doccandidateslist[docname])
-    sizeRel = len(setrelevant)
-    sizeAns = len(setanswer)
-    sizeInt = len(setrelevant.intersection(setanswer))
-    pr = sizeInt/(0.0+sizeAns)
-    re = sizeInt/(0.0+sizeRel)
-    try:
-        f1 = (2*re*pr)/(re+pr)
-    except ZeroDivisionError:
-        f1 = 0
-
-    measures["pr"] = pr
-    measures["re"] = re
-    measures["f1"] = f1
-    measuresdoc[docname] = measures
-
-ap = 0
-apdict = dict()
-for idoc in range(len(docindexnames)):
-    docname = docindexnames[idoc]
-    setrelevant = set(keysfordoc[docname])
-    setanswer = set(doccandidateslist[docname])
-    sizeRel = len(setrelevant)
-    sizeAns = len(setanswer)
-    sizeInt = len(setrelevant.intersection(setanswer))
-    pr = sizeInt/(0.0+sizeAns)
-    re = sizeInt/(0.0+sizeRel)
-    listansweri = []
-    setansweri = set()
-    ap=dict()
-    apsum = 0
-    for term in range(len(doccandidateslist[docname])):
-        listansweri += [doccandidateslist[docname][term]]
-        setanseri = set(listansweri)
-        p = (len(setanseri.intersection(setrelevant)))/(len(setanseri)+0.0)
-        r = int()
-        if doccandidateslist[docname][term] in keysfordoc[docname]:
-            r = 1
-        else:
-            #print docname
-            r = 0
-        ap[term+1] = (p * r) / (sizeRel+0.0)
-        apsum +=  (p * r) / (sizeRel+0.0)
-    apdict[docname] = ap
-    measuresdoc[docname]["map"] = apsum/(0.0+len(doccandidateslist[docname]))
-    ap = 0
-    listanswer = []
-    setansweri = set()
-
-        
-        
