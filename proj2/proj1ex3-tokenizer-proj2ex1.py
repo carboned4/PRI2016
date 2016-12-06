45137741027afba3a776d0bd2976b1182361f9f6
carboned4 <slf450x@hotmail.com>
2016-12-06 17:15:46 +0000

    ex2?

--- proj2/proj1ex3-tokenizer-proj2ex1.py
+++ proj2/proj1ex3-tokenizer-proj2ex1.py
@@ -113,7 +113,7 @@ for filename in os.listdir(path):
 
 
 print "calcular tfidf"
-vectorizer2 = TfidfVectorizer( use_idf=True, tokenizer=my_tokenizer, smooth_idf=True)
+vectorizer2 = TfidfVectorizer( use_idf=True, tokenizer=my_tokenizer)
 docstfidf = vectorizer2.fit_transform(all_docs)
 vecvocab = vectorizer2.vocabulary_
 print "calculado tfidf"
@@ -155,7 +155,7 @@ def checkOrderIsDifferent(ii):
     return sortdifferent
 
 
-top5rankedfordocs = list()
+doccandidateslist = dict()
 
 print "calcular pagerank"
 #calculo de pagerank
@@ -224,25 +224,72 @@ for idoc in range(len(docindexnames)):
     top5indices = sortediterations[lastPRiteration][:5]
     top5ranked = list()
     for itop in top5indices:
-        top5ranked += [(alltermslist[itop],pagerankiterations[lastPRiteration][itop])]
+        #top5ranked += [(alltermslist[itop],pagerankiterations[lastPRiteration][itop])]
+        top5ranked += [alltermslist[itop]]
     
-    top5rankedfordocs += [top5ranked]
-    
-"""
-#calculate tf-idf for each document
-doccandidateslist = dict()
-featurenames = list(vectorizer2.get_feature_names())
-for idoc in range(len(docindexnames)):
     docname = docindexnames[idoc]
-    featurenamescopy = numpy.array(featurenames)
-    tfidfdoccopy = numpy.array(docstfidf.getrow(idoc).toarray()[0])
-    sortedindices = (tfidfdoccopy.argsort()[-5:])[::-1]
-    candidatewordsfordoc = list()
-    for candidatei in sortedindices:
-        candidatewordsfordoc += [featurenamescopy[candidatei]]
-    doccandidateslist[docname] = candidatewordsfordoc
-"""
+    doccandidateslist[docname] = top5ranked
 
 
 #print doccandidateslist
-print top5rankedfordocs
+#print top5rankedfordocs
+
+#ficheiros das equipas
+#get all relevants and merge the relevants for the same documents
+path = "fao30/indexers/iic1/"
+keysfordoc = dict()
+indexerIterator = 1
+setForKeys = set()
+for filename in os.listdir(path):
+    while indexerIterator <= 6:
+        etd = open(path + filename)
+        etdread = etd.read()
+        etdread = etdread.decode('latin-1')
+        etd_keys = etdread.split("\n",-1)[:-1]
+        fname = filename[:-4] + '.txt'
+         
+        setForKeys = setForKeys.union(set(etd_keys))
+        
+        etd_keys = list(setForKeys)
+        keysfordoc[fname] = etd_keys
+        indexerIterator += 1
+        path = path[:-2] + str(indexerIterator) + "/"
+    path = "fao30/indexers/iic1/"
+    indexerIterator = 1
+
+
+
+
+docAPs = dict()
+#calculate AP and therefore MAP
+aptotalsum = 0
+for idoc in range(len(docindexnames)):
+    docname = docindexnames[idoc]
+    setrelevant = set(keysfordoc[docname])
+    setanswer = set(doccandidateslist[docname])
+    sizeRel = len(setrelevant)
+    sizeAns = len(setanswer)
+    sizeInt = len(setrelevant.intersection(setanswer))
+    pr = sizeInt/(0.0+sizeAns)
+    re = sizeInt/(0.0+sizeRel)
+    listansweri = []
+    setansweri = set()
+    apitersum = 0
+    for term in range(len(doccandidateslist[docname])):
+        listansweri += [doccandidateslist[docname][term]]
+        setanseri = set(listansweri)
+        p = (len(setanseri.intersection(setrelevant)))/(len(setanseri)+0.0)
+        r = int()
+        if doccandidateslist[docname][term] in keysfordoc[docname]:
+            r = 1
+        else:
+            r = 0
+        apitersum += (p * r)
+    ap = apitersum  / (sizeRel+0.0)
+    docAPs[docname] = ap
+    aptotalsum += ap
+    listanswer = []
+    setansweri = set()
+
+        
+print "map: " + str(aptotalsum/len(docindexnames))
