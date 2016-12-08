1dc60a5a25e7124fbea729619f75f608a9aa83f8
Miguel Cruz <slf450x@hotmail.com>
2016-12-08 15:55:26 +0000

    vai buscar títulos e descrições

--- proj2/kek.py
+++ proj2/kek.py
@@ -1,9 +1,10 @@
+"""
 from lxml import etree
 import urllib2
 import xml.etree.ElementTree as ET
 import codecs
 
-file=urllib2.urlopen("http://rss.nytimes.com/services/xml/rss/nyt/PersonalTech.xml")
+file=urllib2.urlopen("http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml")
 #etdread = codecs.open(file,'r','utf-8')
 etdread = file.read()
 #etdread = etdread.decode('utf-8')
@@ -20,14 +21,23 @@ for elem in root.findall('.//item//*'):
 
 #root = etree.Element("root")
 #print root.xpath(".//Airport")
-
-
-
 """
+
+
+
 import lxml.etree as etree
 #doc = etree.parse('AirFlightsData.xml')
 doc = etree.parse('http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml')
-for res in doc.xpath('/rss//item//*'):
-    print res.text
+for item in doc.xpath('/rss/channel/item'):
+    #restitle = item.xpath('/item/title')
+    restitle = item.find('title').text
+    resdescription = item.find('description').text
+    #print etree.tostring(item)
+    print "title " + restitle.encode('cp850', errors='replace')
+    print "desc " + resdescription.encode('cp850', errors='replace')
+    #print "title " + restitle.text.encode('cp850', errors='replace')
+    #for resdescription in res.xpath('//description'):
+        #print resdescription.text.encode('cp850', errors='replace')
+        #if resdescription.text is not None:
+            #print "desc " + resdescription.text.encode('cp850', errors='replace')
 #doc.freeDoc()
-"""
